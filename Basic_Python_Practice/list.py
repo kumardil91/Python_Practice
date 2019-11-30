40ecd1c60adb112e0e2430bd61651d92faa83406
Dileep Kumar <dileep.kumar@nectechnologies.in>
2019-11-30 22:11:53 +0900

    Basic Python Practice

---
+++ Basic_Python_Practice/list.py
@@ -0,0 +1,23 @@
+#friends = ["Kevin","kar", "2"]
+
+#print(friends[0:])
+
+#lucky_number = [4,.8,15,16,23,42]
+
+#friends = ["kevin", "dileep", "ashish"]
+#friends.extend(lucky_number)
+#friends.pop()
+#friends.sort()
+
+#print(friends)
+
+
+def sayhi(name,age):
+    print("Hello" + name + ", you are " + age )
+
+
+sayhi("mike","33")
+sayhi("dileep","22")
+#parameter is peace of information
+
+
